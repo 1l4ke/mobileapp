638e1563fb1255e64f459891d95458c449035385
1l4ke <pryakhin.07@gmail.com>
2026-04-15 14:31:50 +0000

    please 2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";  // npm i cors
 import dotenv from "dotenv";
 import authRoutes from "./authRoutes.js";
 
